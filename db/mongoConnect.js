63c056de0902f666b4f0abfaf1a52da5ba033f3b
itaysigro <77383258+itaysigro@users.noreply.github.com>
2021-09-25 15:27:47 +0300

    1

--- db/mongoConnect.js
+++ db/mongoConnect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/users', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/data', {useNewUrlParser: true, useUnifiedTopology: true});
 
 const db = mongoose.connection;
 
